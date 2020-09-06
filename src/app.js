8edde24adac05ffa46b8c31cfab072495038a796
jitendra <jitendra.singh@industrybuying.com>
2020-09-06 14:41:17 +0530

    ready for heruku

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const path = require('path');
 const request = require("request");
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 const publicDirectoryPath = (path.join(__dirname, '../public'));
 app.use(express.static(publicDirectoryPath));
 
-app.get('/about',(req,res)=>{
-    res.send([{name:'Andrew'},{name:'jitendra'}]);
+app.get('/about', (req, res) => {
+    res.send([{ name: 'Andrew' }, { name: 'jitendra' }]);
 })
-app.get('/weather',(req,res)=>{
-    res.send({location:'delhi', forcast:'normal'});
+app.get('/weather', (req, res) => {
+    res.send({ location: 'delhi', forcast: 'normal' });
 })
-app.get('/help',(req,res)=>{
+app.get('/help', (req, res) => {
     res.send('<h1>welcome, happy to</h1>');
 })
-app.listen(3000,()=>{
-    console.log("app is running on the port 300");
+app.listen(port, () => {
+    console.log("app is running on the port" + port);
 })
